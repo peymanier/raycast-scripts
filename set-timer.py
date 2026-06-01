ae7eadcbfc661601ee0bdddf8155cd926df96ee4
Peyman <hassani.peyman627@gmail.com>
2026-06-01 10:55:10 +0330

    Add initial commit

---
+++ set-timer.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Set Timer
+# @raycast.mode inline
+# @raycast.refreshTime 1h
+
+# Optional parameters:
+# @raycast.icon 🤖
+# @raycast.packageName raycast-time-utils
+
+# Documentation:
+# @raycast.description set a timer
+# @raycast.author Peymanier
+# @raycast.authorURL https://github.com/peymanier
+
+print("Hello World!")
+
