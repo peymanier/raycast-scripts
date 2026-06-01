672ec182afba5f15e7e582b91230f418ac731a58
Peyman <hassani.peyman627@gmail.com>
2026-06-01 13:39:19 +0330

    Update shortcut to be able to pass minutes to it

--- start-timer.py
+++ start-timer.py
@@ -4,12 +4,12 @@
 # @raycast.schemaVersion 1
 # @raycast.title time
 # @raycast.mode silent
-# @raycast.refreshTime 1h
+# @raycast.refreshTime 10s
 
 # Optional parameters:
-# @raycast.icon ./images/timer.png
+# @raycast.icon ./images/icons8-timer-50.png
 # @raycast.packageName raycast-scripts
-# # @raycast.argument1 { "type": "text", "placeholder": "minutes" }
+# @raycast.argument1 { "type": "text", "placeholder": "minutes" }
 
 # Documentation:
 # @raycast.description Start a macOS Clock timer
@@ -18,29 +18,36 @@
 
 import subprocess
 import sys
+import tempfile
 
 
 def main() -> None:
-    # if len(sys.argv) < 2:
-    #     print("minutes argument is required", file=sys.stderr)
-    #     sys.exit(1)
-    #
-    # try:
-    #     minutes = int(sys.argv[1])
-    # except ValueError:
-    #     print("minutes must be a number", file=sys.stderr)
-    #     sys.exit(1)
-    #
-    # if minutes <= 0:
-    #     print("minutes must be positive", file=sys.stderr)
-    #     sys.exit(1)
+    if len(sys.argv) < 2:
+        print("minutes argument is required", file=sys.stderr)
+        sys.exit(1)
 
+    try:
+        minutes = int(sys.argv[1])
+    except ValueError:
+        print("minutes must be a number", file=sys.stderr)
+        sys.exit(1)
+
+    if minutes <= 0:
+        print("minutes must be positive", file=sys.stderr)
+        sys.exit(1)
+
+    # https://www.icloud.com/shortcuts/55084635177a4e929b6c43a020065e09
     shortcut_name = "Start Timer"
-    result = subprocess.run(
-        ["shortcuts", "run", shortcut_name],
-        capture_output=True,
-        text=True,
-    )
+
+    with tempfile.NamedTemporaryFile(mode="w", suffix=".txt") as f:
+        f.write(f"{minutes}")
+        f.flush()
+        result = subprocess.run(
+            ["shortcuts", "run", shortcut_name, "-i", f.name],
+            capture_output=True,
+            text=True,
+        )
+
     if result.returncode != 0:
         print(result.stderr, file=sys.stderr)
         sys.exit(1)
